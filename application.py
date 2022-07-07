58894890113fcaacfa8f51c56956d1d3f761560d
LeonLang99 <92875208+LeonLang99@users.noreply.github.com>
2022-07-07 16:02:46 +0200

    Update application.py

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ import pickle
 import requests, zipfile, io
 import os, shutil
 from random import *
-from model import predict_step     
+  
 
 
 
@@ -313,40 +313,4 @@ with st.expander("Here you can try our Image Captoning Program"):
       
       
  
-
-      
-def gen_caption(picture):
-    st.image(picture)
-    st.subheader('Generated caption:')
-    with st.spinner(text='This may take a moment...'):
-        caption = predict_step([picture])
-    st.write(caption[0])
-      
-      
-with st.expander("jetzt wirds wild"):
-#user chooses between preuploaded picture or uploads one himself
-  col1, col2, col3 = st.columns([0.5,1,0.5])
-  with col1:
-    pass
-  with col2:
-    st.subheader('Caption Generator')
-    user_choice = st.radio(label='Choose from either option', options=['Upload your own picture','Image from our dataset'])
-    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-  with col3:
-      pass
-
-
-  if user_choice == 'Upload your own picture':
-    picture = st.file_uploader('', type=['jpg'])
-    if picture != None:
-        gen_caption(picture)
-    else:
-      if st.button('random picture'):
-       st.balloons()
-       z = randint(1, 1000)
-       pic = list(features.keys())[z]
-       image = features[pic].reshape((1,2048))
-       x = plt.imread(images+pic)
-       st.image(x)
-       st.write("Caption:", Image_Caption(image))
     
