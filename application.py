737956bbf42072f14546f7d25b65561699607f75
GitHub42User <92952292+GitHub42User@users.noreply.github.com>
2022-07-07 16:37:57 +0200

    Update application.py

--- application.py
+++ application.py
@@ -283,7 +283,7 @@ with st.expander("Evaluation"):
 with st.expander("Here you can try our Image Captoning Program"): 
   st.write("Try it with a random image by pressing the following Button.")
   if st.button('random picture'):
-       Variable = ["500", "910", "3273", "1863", "1566", "1386", "1355", "1824", "728", "3856", "1453", "763", "1876", "765", "2766", "3041", "3041", "2440", "2286", "4211", "4200", "4502", "4526", "1326", "4122", "1927", "2137", "473", "4489", "3947"]
+       Variable = [500, 910, 3273, 1863, 1566, 1386, 1355, 1824, 728, 3856, 1453, 763, 1876, 765, 2766, 3041, 3041, 2440, 2286, 4211, 4200, 4502, 4526, 1326, 4122, 1927, 2137, 473, 4489, 3947]
        y = randint(0, 29)
        st.write(y)
        z = Variable[y]
