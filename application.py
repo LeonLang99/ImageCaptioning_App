ddd83dd117165250d42432b3cb79ea1d2e97ede5
LeonLang99 <92875208+LeonLang99@users.noreply.github.com>
2022-07-07 13:39:44 +0200

    Update application.py

--- application.py
+++ application.py
@@ -75,8 +75,10 @@ st.title('Image Captioning Group 13')
 
 with st.expander("The Team"):
   st.write("Hello, we are Leon Lang, Jean Louis Fichtner and Loredana Bratu and we created this app as a part of our business informatics course. On this page you will have the opportunity to see how we developed step by step this project, what was our motivation and is our future vision. But before starting, we have a question:  What do you see in the picture bellow?")
+  st.image("https://futalis.de/wp-content/uploads/2020/07/contentbild-hund-fital-1.jpg")
     with st.expander("Possible solution")
-  st.wirte("Most probably you would say “A dog running on a field”, some may say “a dog with white and black spots” and others might say “a dog on a grass and some yellow flowers”. Definitely all of these captions are correct for this image and there may be some more as well. But the point we want to make is that it is so easy for us, as human beings, to just have a look at a picture and describe it in an appropriate language. But, can you write a computer program that takes an image as input and produces a relevant caption as output?")
+      st.write("Caption: ", Image_Caption("https://futalis.de/wp-content/uploads/2020/07/contentbild-hund-fital-1.jpg")
+      st.wirte("Most probably you would say “A dog running on a field”, some may say “a dog with white and black spots” and others might say “a dog on a grass and some yellow flowers”. Definitely all of these captions are correct for this image and there may be some more as well. But the point we want to make is that it is so easy for us, as human beings, to just have a look at a picture and describe it in an appropriate language. But, can you write a computer program that takes an image as input and produces a relevant caption as output?")
   with st.expander("The Mission"):
   st.write("The purpose of our app is to automatically describe an image with one or more natural language sentences. To generate textual descriptions of images we will use Machine Learning and Deep Learning Techniques.")
     
