658be832b9ee82f89d835ea15ab1e2f2e787b1c9
LeonLang99 <92875208+LeonLang99@users.noreply.github.com>
2022-07-07 15:50:24 +0200

    Update application.py

--- application.py
+++ application.py
@@ -302,20 +302,20 @@ with st.expander("Here you can try our Image Captoning Program"):
       
       
       
-with st.expander("jetzt wirds wild")
+with st.expander("jetzt wirds wild"):
 #user chooses between preuploaded picture or uploads one himself
-col1, col2, col3 = st.columns([0.5,1,0.5])
-with col1:
+  col1, col2, col3 = st.columns([0.5,1,0.5])
+  with col1:
     pass
-with col2:
+  with col2:
     st.subheader('Caption Generator')
     user_choice = st.radio(label='Choose from either option', options=['Upload your own picture','Image from our dataset'])
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-with col3:
-    pass
+  with col3:
+      pass
 
 
-if user_choice == 'Upload your own picture':
+  if user_choice == 'Upload your own picture':
     picture = st.file_uploader('', type=['jpg'])
     if picture != None:
         st.write("Caption:", Image_Caption(image))
